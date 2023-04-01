e9cf0ad16a3c7be64077e2ecf1abb7ac81ae021c
vinilemess <viniciulemescaetano@gmail.com>
2023-04-01 17:37:23 -0300

    refatora metodo toString para usar interpolacao de strings do kotlin

--- gilded-rose/src/main/kotlin/com/vinilemess/gildedrose/Item.kt
+++ gilded-rose/src/main/kotlin/com/vinilemess/gildedrose/Item.kt
@@ -2,6 +2,6 @@ package com.vinilemess.gildedrose
 
 open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
-        return this.name + ", " + this.sellIn + ", " + this.quality
+        return "name: $name, sellIn: $sellIn, quality: $quality"
     }
 }
