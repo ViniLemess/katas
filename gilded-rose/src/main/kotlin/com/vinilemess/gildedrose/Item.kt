6cae9fdddde4ed25168b93a61a85992200fde92b
ViniLemess <viniciuslemescaetano@gmail.com>
2023-04-03 00:43:55 -0300

    Corrige logger nao funcional adicionando dependencia logback e trocando import do slf4j

--- gilded-rose/src/main/kotlin/com/vinilemess/gildedrose/Item.kt
+++ gilded-rose/src/main/kotlin/com/vinilemess/gildedrose/Item.kt
@@ -2,6 +2,10 @@ package com.vinilemess.gildedrose
 
 open class Item(var name: String, var sellIn: Int, var quality: Int) {
     override fun toString(): String {
-        return "name: $name, sellIn: $sellIn, quality: $quality"
+        return """
+            name: $name
+            sellIn: $sellIn
+            quality: $quality
+        """.trimIndent()
     }
 }
--- gilded-rose/src/main/kotlin/com/vinilemess/gildedrose/strategies/SulfurasStrategy.kt
+++ gilded-rose/src/main/kotlin/com/vinilemess/gildedrose/strategies/SulfurasStrategy.kt
@@ -1,14 +1,14 @@
 package com.vinilemess.gildedrose.strategies
 
-import com.sun.org.slf4j.internal.LoggerFactory
 import com.vinilemess.gildedrose.Item
+import org.slf4j.LoggerFactory
 
 class SulfurasStrategy: UpdateItemStrategy {
 
     private val log = LoggerFactory.getLogger(SulfurasStrategy::class.java)
 
     override fun updateItem(item: Item): Item {
-        log.debug("No quality updates for the item {}", item)
+        log.info("No quality updates for the item {}", item.name)
         return item
     }
 }
