af5a5b744e92202b866e7c732b853df76dda4a49
vini_lemes <vinicius_caetano@sicredi.com.br>
2023-06-01 08:29:18 -0300

    Cria o livro mágico!

--- enchanting/src/main/kotlin/com/vinilemess/enchanting/MagicBook.kt
+++ enchanting/src/main/kotlin/com/vinilemess/enchanting/MagicBook.kt
@@ -1,6 +1,13 @@
 package com.vinilemess.enchanting
 
+import com.vinilemess.enchanting.enchantment.Enchantment
+import com.vinilemess.enchanting.enchantment.Enchantments
+import kotlin.random.Random
+
 class MagicBook {
 
-
+    private val enchantments = Enchantments.values()
+    fun summonEnchantment(): Enchantment {
+        return enchantments[Random.nextInt(enchantments.size)].enchantment
+    }
 }
