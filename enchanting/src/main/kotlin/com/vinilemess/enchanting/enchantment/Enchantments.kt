a81990be19e62cb70e6e0a5eb0703c46326ecbb8
vini_lemes <vinicius_caetano@sicredi.com.br>
2023-06-08 14:11:40 -0300

    Cria metodo para recuperar nome do encantamento com a primeira letra maiuscula

--- enchanting/src/main/kotlin/com/vinilemess/enchanting/enchantment/Enchantments.kt
+++ enchanting/src/main/kotlin/com/vinilemess/enchanting/enchantment/Enchantments.kt
@@ -1,9 +1,15 @@
 package com.vinilemess.enchanting.enchantment
 
+import com.vinilemess.enchanting.capitalize
+
 enum class Enchantments (val enchantment: Enchantment) {
     ICE(Enchantment("Icy", "+5 ice damage")),
     FIRE(Enchantment("Inferno", "+5 fire damage")),
     LIFESTEAL(Enchantment("Vampire", "+5 lifesteal")),
     AGILITY(Enchantment("Quick", "+5 agility")),
-    STRENGTH(Enchantment("Angry", "+5 strength"))
+    STRENGTH(Enchantment("Angry", "+5 strength"));
+
+    fun getEnchantmentName(): String {
+        return this.name.capitalize()
+    }
 }
---
+++ enchanting/src/main/kotlin/com/vinilemess/enchanting/extensions.kt
@@ -0,0 +1,13 @@
+package com.vinilemess.enchanting
+
+
+fun String.capitalize() :String {
+    val firstCharacter = this.substring(0, 1)
+
+    return this.
+}
+
+
+fun main() {
+    println("osguri".capitalize())
+}
